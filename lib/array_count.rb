103c34051b26635c4b97f492ce74e77186d5c2fc
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-30 18:38:43 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,9 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  count_strings(array)
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  count_empty_strings(array)
 end
